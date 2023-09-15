5fdad334025fb1ef7f0691d0c289f13ee769ab62
nicolasromani <nicolasromani047@gmail.com>
2023-09-15 08:33:15 -0300

    arreglos lateral-over-label y tag-skills

--- JavaScript/script1.js
+++ JavaScript/script1.js
@@ -1,7 +1,13 @@
 const btnCierra = document.querySelector('#closeMark');
+const btnOpen = document.querySelector('#openMark')
 const overLabel = document.querySelector('.over-label__lateral-bar'); 
+
+btnOpen.addEventListener('click', () => {
+    overLabel.style.transform = 'translateX(100%)';
+    overLabel.style.transition = '500ms ease-in-out';
+});
 
 btnCierra.addEventListener('click', () => {
     overLabel.style.transform = 'translateX(-100%)';
-    overLabel.style.transition = '400ms ease-in-out';
+    overLabel.style.transition = '500ms ease-in-out';
 });
